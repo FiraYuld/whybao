0a53adaa624d9ab7e36ff2a253628e43ddae0c5a
FiraYuld <firaspeedcuber@gmail.com>
2026-03-13 22:08:41 +0800

    feat: add NAPSNAPS coat

--- data/brands.ts
+++ data/brands.ts
@@ -1,6 +1,7 @@
 export const brands = [
   { id: "girlyhalo", name: "Girlyhalo", slug: "girlyhalo" },
   { id: "zizifei", name: "ziziFei", slug: "zizifei" },
+  { id: "napsnaps", name: "NAPSNAPS", slug: "napsnaps" },
 ] as const;
 
 export type BrandSlug = (typeof brands)[number]["slug"];
